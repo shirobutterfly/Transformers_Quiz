49d48db7145c9159aaef5779c6654ddb495b939c
Taylor <tayloramccullough@gmail.com>
2016-08-03 14:54:43 -0700

    Done did et!

--- assest/core.js
+++ assest/core.js
@@ -22,12 +22,12 @@ function init(){
 $(document).ready(function(){
  
 //Audio below
-/*myAudio = new Audio('assest/s3song.mp3'); 
+myAudio = new Audio('assest/s3song.mp3'); 
 myAudio.addEventListener('ended', function() {
     this.currentTime = 0;
     this.play();
 }, false);
-myAudio.play();*/
+myAudio.play();
 
 //Below hides all the questions
 $('.questionForm').hide();
@@ -35,8 +35,11 @@ $('.questionForm').hide();
 //show 1st question
 $('#q1').show();
 
+//Going to one q to the next q
+
 $('#q1 #go').click(function(){
 	$('.questionForm').hide();
+	process('q1');
 	$('#q2').fadeIn(500);
 	return false;
 
@@ -44,6 +47,7 @@ $('#q1 #go').click(function(){
 
 $('#q2 #go').click(function(){
 	$('.questionForm').hide();
+	process('q2');
 	$('#q3').fadeIn(500);
 	return false;
 
@@ -51,6 +55,7 @@ $('#q2 #go').click(function(){
 
 $('#q3 #go').click(function(){
 	$('.questionForm').hide();
+	process('q3');
 	$('#q4').fadeIn(500);
 	return false;
 
@@ -58,6 +63,7 @@ $('#q3 #go').click(function(){
 
 $('#q4 #go').click(function(){
 	$('.questionForm').hide();
+	process('q4');
 	$('#q5').fadeIn(500);
 	return false;
 
@@ -65,6 +71,7 @@ $('#q4 #go').click(function(){
 
 $('#q5 #go').click(function(){
 	$('.questionForm').hide();
+	process('q5');
 	$('#q6').fadeIn(500);
 	return false;
 
@@ -72,6 +79,7 @@ $('#q5 #go').click(function(){
 
 $('#q6 #go').click(function(){
 	$('.questionForm').hide();
+	process('q6');
 	$('#q7').fadeIn(500);
 	return false;
 
@@ -79,6 +87,7 @@ $('#q6 #go').click(function(){
 
 $('#q7 #go').click(function(){
 	$('.questionForm').hide();
+	process('q7');
 	$('#q8').fadeIn(500);
 	return false;
 
@@ -86,6 +95,7 @@ $('#q7 #go').click(function(){
 
 $('#q8 #go').click(function(){
 	$('.questionForm').hide();
+	process('q8');
 	$('#q9').fadeIn(500);
 	return false;
 
@@ -93,13 +103,17 @@ $('#q8 #go').click(function(){
 
 $('#q9 #go').click(function(){
 	$('.questionForm').hide();
+	process('q9');
 	$('#q10').fadeIn(500);
 	return false;
 
 });
 
+//Last q goes to call in the results
+
 $('#q10 #go').click(function(){
 	$('.questionForm').hide();
+	process('q10');
 	$('#results').fadeIn(500);
 	return false;
 
@@ -110,6 +124,112 @@ $('#q10 #go').click(function(){
 
 });
 
+
+//Process the answers
+function process(q){
+	if(q == "q1"){
+		var submitted = $('input[name=q1]:checked').val();
+
+			if(submitted == sessionStorage.a1){
+		score ++;
+	}
+
+	}
+
+	if(q == "q2"){
+		var submitted = $('input[name=q2]:checked').val();
+
+			if(submitted == sessionStorage.a2){
+		score ++;
+	}
+
+	}
+
+	if(q == "q3"){
+		var submitted = $('input[name=q3]:checked').val();
+
+			if(submitted == sessionStorage.a3){
+		score ++;
+	}
+
+	}
+
+
+	if(q == "q4"){
+		var submitted = $('input[name=q4]:checked').val();
+
+			if(submitted == sessionStorage.a4){
+		score ++;
+	}
+
+	}
+
+	if(q == "q5"){
+		var submitted = $('input[name=q5]:checked').val();
+
+			if(submitted == sessionStorage.a5){
+		score ++;
+	}
+
+	}
+
+
+	if(q == "q6"){
+		var submitted = $('input[name=q6]:checked').val();
+
+			if(submitted == sessionStorage.a6){
+		score ++;
+	}
+
+	}
+
+
+	if(q == "q7"){
+		var submitted = $('input[name=q7]:checked').val();
+
+			if(submitted == sessionStorage.a7){
+		score ++;
+	}
+
+	}
+
+
+	if(q == "q8"){
+		var submitted = $('input[name=q8]:checked').val();
+
+			if(submitted == sessionStorage.a8){
+		score ++;
+	}
+
+	}
+
+
+
+	if(q == "q9"){
+		var submitted = $('input[name=q9]:checked').val();
+
+			if(submitted == sessionStorage.a9){
+		score ++;
+	}
+
+	}
+
+
+	if(q == "q10"){
+		var submitted = $('input[name=q10]:checked').val();
+
+			if(submitted == sessionStorage.a10){
+		score ++;
+	}
+
+		$('#results').html('<h3> Your final score is:'+score+' out of 10</h3><a href="index.html"> Try Again</a>');
+
+	}
+
+	return false;	
+
+
+}
 
 //Event listener
 window.addEventListener('load', init, false);
